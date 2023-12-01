42ad55e448af249e9259118088addc1c1641f639
rsm-sal072 <sal072@ucsd.edu>
2023-12-01 04:06:48 -0800

    12012023

--- Data.R
+++ Data.R
@@ -1,5 +1,178 @@
-library(tidyverse)
 library(readxl)
-library(writexl)
-library(janitor)
+library(tidyverse)
+library(scales)
+library(stringr)
 
+################# Augie ###################
+
+# Set the working directory
+setwd('/Users/stanlee/Desktop/Desktop - Sangho’s MacBook Pro/UCSD/Class/2023_Fall/MGTA452-Collecting and Analyzing Large Data/Team Project/mgta452_group_c/Augie/Poverty_Data')
+
+# Directory where your Excel files are stored
+directory <- getwd()
+
+# Main categories
+main_categories <- c('AGE', 'SEX', 'RACE AND HISPANIC OR LATINO ORIGIN', 'EDUCATIONAL ATTAINMENT', 'EMPLOYMENT STATUS', 
+                     'WORK EXPERIENCE', 'All Individuals below:', 'ALL INDIVIDUALS WITH INCOME BELOW THE FOLLOWING POVERTY RATIOS', 
+                     'UNRELATED INDIVIDUALS FOR WHOM POVERTY STATUS IS DETERMINED')
+
+# Initialize list to store DataFrames from each file
+dfs <- list()
+
+# Loop through each file in the directory
+file_paths <- list.files(directory, pattern = "\\.xlsx$", full.names = TRUE)
+
+for (file_path in file_paths) {
+  # Read the Excel file from the second tab
+  df <- read_excel(file_path, sheet = 'Data', skip = 2)
+  
+  # Initialize variable to keep track of the current category
+  current_category <- NA
+  
+  # Function to determine the category for each row
+  assign_category <- function(label) {
+    if (label %in% main_categories) {
+      current_category <<- label
+    }
+    return(current_category)
+  }
+  
+  # Assign categories
+  df$Category <- sapply(df$Label, assign_category)
+  
+  # Filter rows and drop columns with 'Margin of Error'
+  df <- df %>% 
+    filter(Label %in% c('Under 18 years', '18 to 64 years', '18 to 34 years', '35 to 64 years', '65 years and over', 
+                        'Male', 'Female', 'Less than high school graduate', 'High school graduate (includes equivalency)',
+                        "Some college, associate's degree", "Bachelor's degree or higher", 'Employed', 'Unemployed', "Asian alone", 
+                        'Black or African American alone', 'White alone', "Native Hawaiian and Other Pacific Islander alone",
+                        "Hispanic or Latino origin (of any race)", 'White', 'American Indian and Alaska Native alone',
+                        'Black or African American', 'American Indian and Alaska Native', 'Asian', 'Native Hawaiian and Other Pacific Islander',
+                        'Hispanic or Latino origin (of any race)', "Worked full-time, year-round in the past 12 months",
+                        "Worked part-time or part-year in the past 12 months", "Did not work")) %>%
+    select(-contains('Margin of Error')) %>%
+    rename(Total_Population = Estimate...2, Below_Poverty = Estimate...4, Percent_Below_Poverty = Estimate...6)
+  
+  # Extract the year from the file name and add as a new column
+  df$Report <- str_extract(basename(file_path), "^[^\\.]+")
+  
+  # Convert columns to integers after removing commas
+  df$Total_Population <- as.integer(gsub(",", "", df$Total_Population))
+  df$Below_Poverty <- as.integer(gsub(",", "", df$Below_Poverty))
+  
+  # Calculate Poverty Rate
+  df$Poverty_Rate <- df$Below_Poverty / df$Total_Population
+  
+  # Add the DataFrame to the list
+  dfs[[length(dfs) + 1]] <- df
+}
+
+# Concatenate all DataFrames in the list
+all_data <- bind_rows(dfs)
+
+# Define the mapping
+label_mapping <- c('White' = 'White', 'White alone' = 'White', 'Black or African American' = 'Black or African American', 
+                   'Black or African American alone' = 'Black or African American', 'American Indian and Alaska Native' = 'American Indian and Alaska Native',
+                   'American Indian and Alaska Native alone' = 'American Indian and Alaska Native', 'Asian' = 'Asian', 
+                   'Asian alone' = 'Asian', 'Native Hawaiian and Other Pacific Islander' = 'Native Hawaiian and Other Pacific Islander',
+                   'Native Hawaiian and Other Pacific Islander alone' = 'Native Hawaiian and Other Pacific Islander', 
+                   'Some other race' = 'Other Race', 'Some other race alone' = 'Other Race', 
+                   'Hispanic or Latino origin (of any race)' = 'Hispanic or Latino')
+
+# Update the 'Label' column
+all_data$Label <- ifelse(all_data$Label %in% names(label_mapping), label_mapping[all_data$Label], all_data$Label)
+
+# Convert 'Report' to datetime and set to the end of the year
+all_data$Year <- ymd(paste0(str_extract(all_data$Report, "\\d{4}"), "-12-31"))
+
+# Fill potentially missing data
+all_data$Total_Population[is.na(all_data$Total_Population)] <- 0
+all_data$Below_Poverty[is.na(all_data$Below_Poverty)] <- 0
+
+# Additional processing if needed
+all_data$Poverty_Rate <- all_data$Below_Poverty / all_data$Total_Population
+
+# functions to display ,
+print_number_with_commas <- function(number) {
+  formatted_number <- format(number, big.mark = ",", scientific = FALSE)
+  cat(formatted_number, "\n")
+}
+
+all_data %>% 
+  mutate(Poverty_Rate = round(Poverty_Rate, 2)) %>% 
+  relocate(Report, Year, Category, Label, Poverty_Rate, Percent_Below_Poverty, Below_Poverty, Total_Population) -> all_data
+
+
+# Subsetting data for high-level employment status
+all_data_employment_highlevel <- all_data %>%
+  filter(Category == 'EMPLOYMENT STATUS' & Label %in% c('Employed', 'Unemployed'))
+
+# Subsetting data for low-level work experience
+all_data_employment_lowlevel <- all_data %>%
+  filter(Category == 'WORK EXPERIENCE' & Label %in% c('Worked full-time, year-round in the past 12 months', 
+                                                      'Worked part-time or part-year in the past 12 months', 
+                                                      'Did not work'))
+
+
+
+
+
+plot_trend <- function(data, category, title) {
+  filtered_data <- data %>% filter(Category == category)
+  
+  ggplot(filtered_data, aes(x = Year, y = Poverty_Rate, color = Label)) +
+    geom_line() +
+    geom_text(aes(label = sprintf("%.0f%%", Poverty_Rate*100)), 
+              vjust = -0.5, hjust = 1.5, 
+              size = 5, fontface = "bold") +
+    labs(title = title, x = 'Year', y = '% Below Poverty Line') +
+    scale_y_continuous(labels = scales::percent_format(scale = 100)) +
+    scale_color_brewer(palette = "Set1") +
+    theme_classic() +
+    theme(
+      axis.text.x = element_text(angle = 45, hjust = 1, size = 12, face = "bold"),
+      axis.text.y = element_text(size = 15, face = "bold"),
+      axis.title.x = element_text(size = 17, face = "bold"),
+      axis.title.y = element_text(size = 17, face = "bold"),
+      legend.title = element_text(size = 15, face = "bold"),
+      legend.text = element_text(size = 15, face = "bold"),
+      plot.title = element_text(size = 24, face = "bold", hjust = 0.5)
+    )
+  
+}
+
+# Example usage
+plot_trend(all_data, 'SEX', 'US Poverty Rate by Gender')
+plot_trend(all_data, 'AGE', 'US Poverty Rate by Age')
+plot_trend(all_data, 'EDUCATIONAL ATTAINMENT', 'US Poverty Rate by Education Attainment')
+plot_trend(all_data_employment_highlevel, 'EMPLOYMENT STATUS', 'US Poverty Rate by Employment Status')
+plot_trend(all_data_employment_lowlevel, 'WORK EXPERIENCE', 'US Poverty Rate by Work Experience')
+plot_trend(all_data, 'RACE AND HISPANIC OR LATINO ORIGIN', 'US Poverty Rate by Race')
+
+
+
+# Subset data for race and Hispanic or Latino origin for the year 2022
+race_df <- all_data %>% 
+  filter(Category == 'RACE AND HISPANIC OR LATINO ORIGIN', lubridate::year(Year) == 2022)
+
+# Calculate total population and total in poverty
+total_population <- sum(race_df$Total_Population, na.rm = TRUE)
+total_in_poverty <- sum(race_df$Below_Poverty, na.rm = TRUE)
+
+# Calculate percentages
+race_df$Race_to_Total_Population <- race_df$Total_Population / total_population
+race_df$Race_to_Total_in_Poverty <- race_df$Below_Poverty / total_in_poverty
+
+# Reshape the DataFrame
+melted_data <- tidyr::pivot_longer(race_df, 
+                                   cols = c(Race_to_Total_Population, Race_to_Total_in_Poverty),
+                                   names_to = 'Type', 
+                                   values_to = 'Percentage')
+
+ggplot(melted_data, aes(x = Label, y = Percentage, fill = Type)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
+  labs(title = 'Comparison of Race Representation in Total Population vs Total in Poverty',
+       x = 'Race', y = 'Percentage') +
+  scale_fill_brewer(palette = "Pastel1") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
--- shiny.R
+++ shiny.R
@@ -1,5 +1,11 @@
 library(shiny)
 library(shinythemes)
+library(shinyWidgets)
+library(DT)
+
+
+
+
 
 ui <- fluidPage(
   theme = shinythemes::shinytheme("flatly"),
@@ -67,10 +73,36 @@ ui <- fluidPage(
                       h5("2. *How will the aging population affect the job market by 2030?* - Predicting shifts in employment sectors due to an increasing number of retirees."),
                       h6("Currently, there are only two bullet points based on Augie's questions. More detailed content and analytical visualizations will be added later")),
              tabPanel("Descriptive Analysis",
-                      h3("** Descriptive Analysis **"),
-                      h5("1. *Which state has the most unusual ratio of people to domestic pets?* - Investigating the correlation between human population and pet ownership."),
-                      h5("2. *Is there a state where people have an unusually high number of hobbies or leisure activities?* - Analyzing census data on leisure activities and comparing across states."),
-                      h6("Currently, there are only two bullet points based on Augie's questions. More detailed content and analytical visualizations will be added later")),
+                      tabsetPanel(
+                        tabPanel("Introduction",
+                                 h3("** Descriptive Analysis **"),
+                                 h5("1. *Which state has the most unusual ratio of people to domestic pets?* - Investigating the correlation between human population and pet ownership."),
+                                 h5("2. *Is there a state where people have an unusually high number of hobbies or leisure activities?* - Analyzing census data on leisure activities and comparing across states."),
+                                 h6("Currently, there are only two bullet points based on Augie's questions. More detailed content and analytical visualizations will be added later")),
+                        tabPanel("Data",
+                                 br(),
+                                 fluidRow(
+                                   column(3, pickerInput("filterReport", "Report", choices = NULL, multiple = TRUE, options = list(`actions-box` = TRUE))),
+                                   column(3, pickerInput("filterCategory", "Category", choices = NULL, multiple = TRUE, options = list(`actions-box` = TRUE))),
+                                   column(3, pickerInput("filterLabel", "Label", choices = NULL, multiple = TRUE, options = list(`actions-box` = TRUE)))
+                                 ),
+                                 DT::dataTableOutput("dataView"),
+                                 downloadButton("downloadData", "Download Data")
+                        ),
+                        tabPanel("Plot_Trend",
+                                 br(),
+                                 pickerInput("selectCategory", "Select Category", 
+                                             choices = c("SEX", "AGE", "EDUCATIONAL ATTAINMENT", 
+                                                         "EMPLOYMENT STATUS", "WORK EXPERIENCE", 
+                                                         "RACE AND HISPANIC OR LATINO ORIGIN")),
+                                 plotOutput("trendPlot", height = "800px", width = "100%")),
+                        tabPanel("Plot_Race",
+                                 br(),
+                                 plotOutput("racePlot", height = "800px", width = "100%")
+                        )
+                      )),
+             
+             
              tabPanel("Regression Analysis",
                       h3("** Regression Analysis **"),
                       h5("1. *Do states with higher numbers of pet owners have lower stress-related health issues?* - Exploring the correlation between pet ownership (from other sources) and health data from the census."),
@@ -80,6 +112,133 @@ ui <- fluidPage(
   )
 )
 
-server <- function(input, output) { }
+server <- function(input, output, session) {
+  
+  # Define the path to Data.R
+  data_file_path <- "/Users/stanlee/Desktop/Desktop - Sangho’s MacBook Pro/UCSD/Class/2023_Fall/MGTA452-Collecting and Analyzing Large Data/Team Project/mgta452_group_c/Data.R"
+  
+  
+  
+  
+  # Check if the file exists before sourcing
+  if (file.exists(data_file_path)) {
+    source(data_file_path)
+  } else {
+    stop("The file Data.R was not found in the specified path: ", data_file_path)
+  }
+  
+  
+  
+  
+  observe({
+    updatePickerInput(session, "filterReport", 
+                      choices = sort(unique(all_data$Report)),
+                      selected = all_data$Report)
+    updatePickerInput(session, "filterCategory", 
+                      choices = sort(unique(all_data$Category)),
+                      selected = all_data$Category)
+    updatePickerInput(session, "filterLabel", 
+                      choices = sort(unique(all_data$Label)),
+                      selected = all_data$Label)
+    
+  })
+  
+  output$dataView <- DT::renderDataTable({
+    filtered_data <- all_data
+    
+    if (!is.null(input$filterReport)) {
+      filtered_data <- filtered_data[filtered_data$Report %in% input$filterReport, ]
+    }
+    if (!is.null(input$filterCategory)) {
+      filtered_data <- filtered_data[filtered_data$Category %in% input$filterCategory, ]
+    }
+    if (!is.null(input$filterLabel)) {
+      filtered_data <- filtered_data[filtered_data$Label %in% input$filterLabel, ]
+    }
+    if (!is.null(input$filterYear)) {
+      filtered_data <- filtered_data[all_data$Year >= input$filterYear[1] & all_data$Year <= input$filterYear[2], ]
+    }
+    
+    datatable <- DT::datatable(filtered_data,
+                               extensions = c("Buttons", "FixedHeader"), 
+                               options = list(pageLength = 50,
+                                              dom = "Blfrtip",
+                                              buttons = c("copy", "csv", "excel"),
+                                              scrollX = TRUE,
+                                              scrollY = "700px",
+                                              fixedHeader = TRUE,
+                                              fixedColumns = list(leftColumns = 2)),
+                               rownames = FALSE) %>%
+      formatStyle('Report', backgroundColor = styleEqual(levels = unique(filtered_data$Report), values = rep("lightyellow", length(unique(filtered_data$Report)))))
+    
+    datatable
+    
+  })
+  
+  
+  
+  
+  output$trendPlot <- renderPlot({
+    category <- input$selectCategory
+    title <- paste("US Poverty Rate by", category)
+    
+    data_to_plot <- all_data 
+    
+    if (category == "EMPLOYMENT STATUS") {
+      data_to_plot <- all_data_employment_highlevel
+    } else if (category == "WORK EXPERIENCE") {
+      data_to_plot <- all_data_employment_lowlevel
+    }
+    
+    plot_trend(data_to_plot, category, title)
+  })
+  
+  
+  
+  
+  output$racePlot <- renderPlot({
+    # Recreate melted_data using the entire race_df
+    melted_data <- tidyr::pivot_longer(race_df, 
+                                       cols = c(Race_to_Total_Population, Race_to_Total_in_Poverty),
+                                       names_to = 'Type', 
+                                       values_to = 'Percentage')
+    
+    # Plotting code
+    ggplot(melted_data, aes(x = Label, y = Percentage, fill = Type)) +
+      geom_bar(stat = "identity", position = position_dodge()) +
+      geom_text(aes(label = sprintf("%.2f%%", Percentage*100)),  
+                vjust = -0.5, position = position_dodge(width = 0.9),
+                size = 5,  
+                fontface = "bold") +  
+      labs(title = 'Comparison of Race Representation in Total Population vs Total in Poverty in 2022',
+           x = 'Race', y = 'Percentage') +
+      scale_fill_manual(values = c("darkblue", "darkorange")) +
+      scale_y_continuous(labels = scales::percent_format(accuracy = 0.01)) +
+      theme_classic() +
+      theme(
+        axis.text.x = element_text(angle = 45, hjust = 1, size = 12, face = "bold"),
+        axis.text.y = element_text(size = 15, face = "bold"),
+        axis.title.x = element_text(size = 17, face = "bold"),
+        axis.title.y = element_text(size = 17, face = "bold"),
+        legend.text = element_text(size = 15, face = "bold"),
+        plot.title = element_text(size = 24, face = "bold", hjust = 0.5)
+      )
+    
+  })
+  
+  
+  
+  
+  
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste("all_data-", Sys.Date(), ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(all_data, file, row.names = FALSE)
+    })
+  
+  
+}
 
 shinyApp(ui = ui, server = server)
