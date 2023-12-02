1db21a9e96fcb4c9ff17b90d09eccb4b83d0a730
rsm-sal072 <sal072@ucsd.edu>
2023-12-02 02:42:36 -0800

    12012023

--- shiny.R
+++ shiny.R
@@ -5,14 +5,12 @@ library(DT)
 
 
 
-
-
 ui <- fluidPage(
   theme = shinythemes::shinytheme("flatly"),
   div(class = "title-style", 
-      tags$h1("MGTA 452 Collecting and Analyzing Large Data (We will rename this later)", 
+      tags$h1("MGTA 452 Collecting and Analyzing Large Data", 
               style = "text-align: center; font-size: 32px; margin-bottom: 30px;")), 
-  h2("US Census Data Analysis Project (Group C). (We will rename this later)", 
+  h2("US Census Data Analysis Project (Group C)", 
      style = "text-align: center; font-size: 20px; margin-bottom: 20px;"), 
   div(style = "text-align: right;", 
       tags$img(src = "rady_logo.png", height = 100, width = 430)),
@@ -21,52 +19,109 @@ ui <- fluidPage(
                       tabsetPanel(
                         tabPanel("Project Topic Introduction",
                                  style = "margin-top: 20px;",
-                                 p("This is where we are going to introduce our Data; US Census data and explain what we are aim to accomplish throughout this project")
+                                 fluidRow(
+                                   column(6,  
+                                          p(style = "font-size: 20px; line-height: 1.6; text-align: justify; font-family: Arial, sans-serif; font-weight: 500;",
+                                            "- In this project, we delve into an in-depth analysis of the US Census data, a rich repository of demographic, social, and economic information.",
+                                            br(), 
+                                            br(),
+                                            "- Our goal is to uncover meaningful insights into the socio-economic landscape of the United States.",
+                                            br(),
+                                            br(),
+                                            "- We aim to explore key trends and patterns related to employment, poverty, and demographic changes.",
+                                            br(),
+                                            br(),
+                                            "- By leveraging advanced analytical techniques, our analysis will not only highlight current socio-economic conditions but also project future trends.",
+                                            br(),
+                                            br(),
+                                            "- This exploration is crucial for understanding the dynamics of various demographic groups and informing policy decisions.",
+                                            br(),
+                                            br(),
+                                            "- Ultimately, this project seeks to provide a comprehensive understanding of the intricate tapestry of American society as depicted through the lens of the Census data.")
+                                   ),
+                                   column(6, 
+                                          img(src = "us_census.png", height = "80%", width = "100%"),
+                                          p(style = "font-size: 16px; font-family: 'Times New Roman', serif; text-align: center; font-style: italic; margin-top: 10px;",
+                                            "Source: ",
+                                            a(href = "https://policylab.rutgers.edu/uscensus/", "https://policylab.rutgers.edu/uscensus/", target = "_blank")
+                                          )
+                                   )
+                                 )
+                                 
                         ),
                         tabPanel("Group C Introduction",
                                  style = "margin-top: 20px;",
-                                 tags$style(type="text/css", 
-                                            ".team-member {display: flex; align-items: center; margin-bottom: 20px; text-align: left;}",
-                                            ".team-member img {margin-right: 15px;}",
-                                            ".team-member .info {text-align: left;}",
-                                            ".team-member .info h3, .team-member .info p {margin: 0;}",
+                                 tags$style(type = "text/css", 
+                                            ".team-grid {display: grid; grid-template-columns: repeat(auto-fill, minmax(300px, 1fr)); gap: 20px; padding: 20px;}",
+                                            ".team-member {text-align: center;}",
+                                            ".team-member img {height: 200px; width: 200px; border-radius: 50%; margin-bottom: 10px; object-fit: cover;}",
+                                            ".team-member .info h3 {margin: 0; font-size: 20px; font-weight: bold;}",
                                             ".team-member .info p {font-size: 16px;}"),
-                                 div(class = "container", 
+                                 h2("Meet Group C", style = "text-align: center; margin-bottom: 30px;"),
+                                 div(class = "team-grid",
                                      div(class = "team-member",
-                                         img(src = "Augie.png", height = 400, width = 300, alt = "Augie"),
+                                         img(src = "Augie.png", alt = "Augie"),
                                          div(class = "info",
                                              h3("Augustine Donovan"),
                                              p("audonovan@ucsd.edu")
-                                         )),
+                                         )
+                                     ),
                                      div(class = "team-member",
-                                         img(src = "Alan.png", height = 400, width = 300, alt = "Alan"),
+                                         img(src = "Alan.png", alt = "Alan"),
                                          div(class = "info",
                                              h3("Alan Shapiro"),
                                              p("a1shapiro@ucsd.edu")
-                                         )),
+                                         )
+                                     ),
                                      div(class = "team-member",
-                                         img(src = "Robin.png", height = 400, width = 300, alt = "Robin"),
+                                         img(src = "Robin.png", alt = "Robin"),
                                          div(class = "info",
                                              h3("Robin Reese"),
-                                             p("Robin.Reese@rady.ucsd.edu", style = "font_size: 16px;")
-                                         )),
+                                             p("Robin.Reese@rady.ucsd.edu")
+                                         )
+                                     ),
                                      div(class = "team-member",
-                                         img(src = "Sangho.png", height = 400, width = 300, alt = "Sangho"),
+                                         img(src = "Sangho.png", alt = "Sangho"),
                                          div(class = "info",
                                              h3("Sangho Lee"),
-                                             p("sal072@ucsd.edu", style = "font-size: 16px;")
-                                         )),
+                                             p("sal072@ucsd.edu")
+                                         )
+                                     ),
                                      div(class = "team-member",
-                                         img(src = "Shifali.png", height = 400, width = 300, alt = "Shifali"),
+                                         img(src = "Shifali.png", alt = "Shifali"),
                                          div(class = "info",
                                              h3("Shefali Sinha"),
                                              p("s3sinha@ucsd.edu")
-                                         ))
-                                 )
-                        )
+                                         ))))
+                        
+                        
+                      )),
+             tabPanel("Index",
+                      div(class = "index-content", 
+                          tags$style(type = "text/css", 
+                                     ".index-content {text-align: left; margin-top: 20px; font-family: Arial, sans-serif; padding-left: 20px;}",
+                                     ".index-main {font-weight: bold; font-size: 18px; margin-top: 15px;}",
+                                     ".index-sub {margin-left: 20px; font-size: 16px; margin-top: 5px;}"),
+                          h3("Application Index", style = "margin-bottom: 30px; "),
+                          
+                          div(class = "index-main", "Predictive Analysis"),
+                          div(class = "index-sub", "1. Demographic makeup of US cities"),
+                          div(class = "index-sub", "2. Aging population and job market"),
+                          
+                          div(class = "index-main", "Descriptive Analysis"),
+                          div(class = "index-sub", "Introduction: Overview of the data sources and objectives of the descriptive analysis."),
+                          div(class = "index-sub", "Data: Interactive exploration of the dataset with various filters and search options."),
+                          div(class = "index-sub", "Plot (Trend): Visualization of key trends in the data, such as poverty rates and employment patterns."),
+                          div(class = "index-sub", "Plot (Race): Graphical representation comparing racial demographics in the context of poverty and population statistics."),
+                          
+                          div(class = "index-main", "Regression Analysis"),
+                          div(class = "index-sub", "1. Pet ownership and health issues"),
+                          div(class = "index-sub", "2. Fast-food restaurants and single-person households"),
+                          
+                          div(class = "index-main", "Conclusion")
                       )
-             ),
-             tabPanel("Index"),
+             )
+             ,
              tabPanel("Predictive Analysis",
                       h3("** Predictive Analysis **"),
                       h5("1. *What will be the demographic makeup of major US cities in 10 years?* - Projecting future changes in ethnicity, age distribution, and migration patterns."),
@@ -75,10 +130,24 @@ ui <- fluidPage(
              tabPanel("Descriptive Analysis",
                       tabsetPanel(
                         tabPanel("Introduction",
-                                 h3("** Descriptive Analysis **"),
-                                 h5("1. *Which state has the most unusual ratio of people to domestic pets?* - Investigating the correlation between human population and pet ownership."),
-                                 h5("2. *Is there a state where people have an unusually high number of hobbies or leisure activities?* - Analyzing census data on leisure activities and comparing across states."),
-                                 h6("Currently, there are only two bullet points based on Augie's questions. More detailed content and analytical visualizations will be added later")),
+                                 br(),
+                                 h3("** Descriptive Analysis of Poverty and Employment Data **"),
+                                 h4("This section provides insights into various socioeconomic indicators across different demographic groups.", style = "color: #800000;"),
+                                 h4("1. *Employment Status and Poverty Rates* - Explore the relationship between different employment statuses and poverty rates."),
+                                 h4("2. *Work Experience and Poverty Trends* - Analyze how various levels of work experience correlate with poverty trends."),
+                                 h5(HTML("<i>The analysis leverages data on employment status, work experience, and poverty rates across different demographic categories.</i>"), style = "color: #006400;"),
+                                 br(),
+                                 br(),
+                                 h3("** Trends in Poverty and Employment **"),
+                                 h4("Visualize and analyze trends in poverty rates across different employment statuses and work experiences."),
+                                 h4("Compare these trends over the years to understand how socio-economic factors have evolved."),
+                                 h5(HTML("<i>Interactive visualizations allow for detailed exploration of the data.</i>"), style = "color: #006400;"), 
+                                 br(),
+                                 br(),
+                                 h3("** Race, Population, and Poverty **"),
+                                 h4("Explore the representation of different racial groups in the total population and among those in poverty."),
+                                 h4("This analysis provides a nuanced view of racial disparities in socioeconomic status."),
+                                 h5(HTML("<i>Data visualizations highlight the differences in percentage representation across racial groups.</i>"), style = "color: #006400;")),
                         tabPanel("Data",
                                  br(),
                                  fluidRow(
@@ -89,14 +158,14 @@ ui <- fluidPage(
                                  DT::dataTableOutput("dataView"),
                                  downloadButton("downloadData", "Download Data")
                         ),
-                        tabPanel("Plot_Trend",
+                        tabPanel("Plot (Trend)",
                                  br(),
                                  pickerInput("selectCategory", "Select Category", 
                                              choices = c("SEX", "AGE", "EDUCATIONAL ATTAINMENT", 
                                                          "EMPLOYMENT STATUS", "WORK EXPERIENCE", 
                                                          "RACE AND HISPANIC OR LATINO ORIGIN")),
                                  plotOutput("trendPlot", height = "800px", width = "100%")),
-                        tabPanel("Plot_Race",
+                        tabPanel("Plot (Race)",
                                  br(),
                                  plotOutput("racePlot", height = "800px", width = "100%")
                         )
